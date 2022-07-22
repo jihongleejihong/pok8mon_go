7cf7cd15d80a0a43f5eb1e500bc8001c64c3830f
jihongleejihong <stellamea@yonsei.ac.kr>
2022-07-22 15:07:18 +0900

    add ohe.joblib, clf_label.csv, and cache for models

--- pages/1-모델 성능 테스트해보기.py
+++ pages/1-모델 성능 테스트해보기.py
@@ -7,21 +7,24 @@ import cv2
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import os
 import json
+import joblib
 
 model_path = 'resources/fitted_models/'
-clf_label = ['Weezing', 'Magneton', 'Horsea', 'Rhydon', 'Meowth', 'Weedle', 'Machoke', 'Ivysaur', 'Vulpix', 'Snorlax', 'Persian', 'Rhyhorn', 'Cloyster', 'Mew', 'Poliwrath', 'Muk', 'Wartortle', 'Haunter', 'Koffing', 'Drowzee', 'Clefairy', 'Golem', 'Grimer', 'Venomoth', 'Venonat', 'Gastly', 'Gyarados', 'Omastar', 'Sandslash', 'Pidgey', 'Lickitung', 'Geodude', 'Raticate', 'Oddish', 'Dragonair', 'Hypno', 'Pidgeot', 'Charmeleon', 'Beedrill', 'Rapidash', 'Nidorina', 'Shellder', 'Jynx', 'Kingler', 'Poliwhirl', 'Arcanine', 'Diglett', 'Magikarp', 'Bellsprout', 'Eevee', 'Slowpoke', 'Caterpie', 'Golbat', 'Alolan Sandslash', 'Aerodactyl', 'Growlithe', 'Spearow', 'Venusaur', 'Psyduck', 'Gengar', 'Kadabra', 'Dragonite', 'Sandshrew', 'Farfetchd', 'Golduck', 'Metapod', 'Marowak', 'Kakuna', 'Arbok', 'Ditto', 'Gloom', 'Blastoise', 'Krabby', 'Jigglypuff', 'Machamp', 'Seadra', 'Hitmonchan', 'Porygon', 'Onix', 'Electrode', 'Pikachu', 'Alakazam', 'Vaporeon', 'Hitmonlee', 'Charizard', 'Mankey', 'Doduo', 'Fearow', 'Zubat', 'Staryu', 'Butterfree', 'Slowbro', 'Victreebel', 'Squirtle', 'Pidgeotto', 'Tentacool', 'Parasect', 'Bulbasaur', 'Omanyte', 'Clefable', 'Moltres', 'Zapdos', 'Primeape', 'Kabuto', 'Scyther', 'Nidoqueen', 'Dratini', 'Electabuzz', 'Nidorino', 'Exeggcute', 'Chansey', 'Pinsir', 'Tentacruel', 'Paras', 'Graveler', 'Vileplume', 'Articuno', 'Ponyta', 'Rattata', 'Magnemite', 'Flareon', 'Tangela', 'Mewtwo', 'Cubone', 'Starmie', 'Weepinbell', 'Lapras', 'Raichu', 'Seel', 'Dodrio', 'Dugtrio', 'Magmar', 'Kabutops', 'MrMime', 'Seaking', 'Voltorb', 'Tauros', 'Abra', 'Nidoking', 'Poliwag', 'Charmander', 'Ekans', 'Jolteon', 'Ninetales', 'Wigglytuff', 'Dewgong', 'Machop', 'Kangaskhan', 'Goldeen', 'Exeggutor']
-
+clf_label = pd.read_csv("resources/clf_label.csv")["0"].to_list()
 
 st.title("준비된 포켓몬 이미지를 넣어서 모델성능을 테스트 해보기")
-
-
 
 # msg = "Now loading models"
 
 # st.write(msg)
-clf_model = tf.keras.models.load_model(f'{model_path}model_classification.h5')
-# type1_model = tf.keras.models.load_model(f'{model_path}resnet50_type1.h5')
-# type2_model = tf.keras.models.load_model(f'{model_path}resnet50_type2.h5')
+@st.experimental_singleton
+def load_models(model_path):
+    clf_model = tf.keras.models.load_model(f'{model_path}model_classification.h5')
+    type1_model = tf.keras.models.load_model(f'{model_path}resnet50_type1.h5')
+    type2_model = tf.keras.models.load_model(f'{model_path}resnet50_type2.h5')
+    return clf_model, type1_model, type2_model
+
+clf_model, type1_model, type2_model = load_models(model_path)
 
 # msg = ""
 
@@ -63,3 +66,15 @@ for i in y_predict[0]:
         )    
     st.write(poke_dict["kor_name"][clf_label[i]], end=' ')
     st.write("일 확률",format(y_pred[0][i]*100, '.2f'),"% 입니다.")
+
+
+pred_type1 = type1_model.predict(img_to_4D(test_img))
+pred_type2 = type2_model.predict(img_to_4D(test_img))
+
+st.write(pred_type1)
+st.write(pred_type2)
+
+ohe = joblib.load("resources/fitted_models/ohe.joblib")
+
+st.write(ohe.inverse_transform(pred_type1))
+st.write(ohe.inverse_transform(pred_type2))
