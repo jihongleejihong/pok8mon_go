72eca23748626e2b6ac2bce0b21fe67f60ecf779
jihongleejihong <stellamea@yonsei.ac.kr>
2022-07-22 16:17:58 +0900

    add test img, update requirements

--- pages/1-모델 성능 테스트해보기.py
+++ pages/1-모델 성능 테스트해보기.py
@@ -5,16 +5,15 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import cv2
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-import os
 import json
 import joblib
+import glob
+
 
 model_path = 'resources/fitted_models/'
 clf_label = pd.read_csv("resources/clf_label.csv")["0"].to_list()
 
 st.title("준비된 포켓몬 이미지를 넣어서 모델성능을 테스트 해보기")
-
-# msg = "Now loading models"
 
 # st.write(msg)
 @st.experimental_singleton
@@ -26,7 +25,6 @@ def load_models(model_path):
 
 clf_model, type1_model, type2_model = load_models(model_path)
 
-# msg = ""
 
 with open('resources/poke_dict.json', 'r') as fp:
    poke_dict = json.load(fp)
@@ -40,11 +38,15 @@ def read_and_resize_img(path, size = None, show = False):
         plt.imshow(img)
     return img
 
+test_imgs = glob.glob("resources/img/test_img/*")
 
-test_img = read_and_resize_img('resources/img/test_img/IMG_9536.jpeg', size = (300, 300))
+test_idx = st.select_slider('테스트 하고 싶은 포켓몬을 골라보세요', range(len(test_imgs)))
 
 
-st.write('테스트 이미지')
+glob.glob("resource")
+
+test_img = read_and_resize_img(test_imgs[test_idx], size = (300, 300))
+
 st.image(test_img)
 
 def img_to_4D(img):
@@ -57,10 +59,11 @@ def img_to_4D(img):
 
 y_pred = clf_model.predict(img_to_4D(test_img))
 
-y_predict = np.argsort(-y_pred)[:,:5]
+y_predict = np.argsort(-y_pred)[:,:3]
 
-from PIL import Image
+
 for i in y_predict[0]:
+    
     st.image(f"https://assets.pokemon.com/assets/cms2/img/pokedex/full/{poke_dict['idx'][clf_label[i]]}.png",
             width=150, # Manually Adjust the width of the image as per requirement
         )    
@@ -68,8 +71,9 @@ for i in y_predict[0]:
     st.write("일 확률",format(y_pred[0][i]*100, '.2f'),"% 입니다.")
 
 
-pred_type1 = type1_model.predict(img_to_4D(test_img))
-pred_type2 = type2_model.predict(img_to_4D(test_img))
+
+pred_type1 = type1_model.predict(img_to_4D(test_img) * 255)
+pred_type2 = type2_model.predict(img_to_4D(test_img) * 255)
 
 st.write(pred_type1)
 st.write(pred_type2)
